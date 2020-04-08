a8c1949a51143dba5de28cb286d6c66340bc8bca
vinuch <vincentedeh42@gmail.com>
2020-04-08 01:03:20 -0700

    attempted assesment challenge 1 - 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,30 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: this.data.reportedCases * 10,
+      infectionsByRequestedTime: this.impact.currentlyInfected * (2 ** 10),
+      severeCasesByRequestedTime: 0.15 * this.impact.infectionsByRequestedTime,
+      hospitalBedsByRequestedTime: 0.35 * this.data.totalHospitalBeds,
+      casesForICUByRequestedTime: 0.5 * this.impact.infectionsByRequestedTime,
+      casesForVentilatorsByRequestedTime: 0.2 * this.severeImpact.infectionsByRequestedTime,
+      dollarsInFlight:
+        this.impact.infectionsByRequestedTime * this.data.region.avgDailyIncomePopulation
+        * this.data.region.avgDailyIncomeInUSD * this.data.timeToElapse
+    },
+    severeImpact: {
+      currentlyInfected: this.data.reportedCases * 50,
+      infectionsByRequestedTime: this.impact.currentlyInfected * (2 ** 10),
+      severeCasesByRequestedTime: 0.15 * this.severeImpact.infectionsByRequestedTime,
+      hospitalBedsByRequestedTime: 0.35 * this.data.totalHospitalBeds,
+      casesForICUByRequestedTime: 0.5 * this.severeImpact.infectionsByRequestedTime,
+      casesForVentilatorsByRequestedTime: 0.2 * this.severeImpact.infectionsByRequestedTime,
+      dollarsInFlight:
+        this.severeImpact.infectionsByRequestedTime * this.data.region.avgDailyIncomePopulation
+        * this.data.region.avgDailyIncomeInUSD * this.data.timeToElapse
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
