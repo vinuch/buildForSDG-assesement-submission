a678e860ba13900c389b37bd901172e808a42dd9
vinuch <vincentedeh42@gmail.com>
2020-04-09 04:03:47 -0700

    removed the convert to days function

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,10 @@
-function convertToDays(periodType, time) {
-  let result;
-  if (periodType === 'weeks') {
-    result = time * 7;
-  } else if (periodType === 'months') {
-    result = time * 30;
-  } else if (periodType === 'days') {
-    result = time;
-  }
-  return result;
-}
-
 const covid19ImpactEstimator = (data) => {
   const input = data;
-  input.timeToElapse = convertToDays(input.periodType, input.timeToElapse);
   return {
     data: input,
     impact: {
       currentlyInfected: input.reportedCases * 10,
-      get infectionsByRequestedTime() { return this.currentlyInfected * (2 ** 10); },
+      get infectionsByRequestedTime() { return this.currentlyInfected * (2 ** 2.3); },
       get severeCasesByRequestedTime() { return 0.15 * this.infectionsByRequestedTime; },
       hospitalBedsByRequestedTime: 0.35 * input.totalHospitalBeds,
       get casesForICUByRequestedTime() { return 0.5 * this.infectionsByRequestedTime; },
