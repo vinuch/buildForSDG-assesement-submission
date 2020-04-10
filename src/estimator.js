56940638b3ce43cae9d4cfc7055857edd71308fb
vinuch <vincentedeh42@gmail.com>
2020-04-09 17:56:14 -0700

    5% is 0.05 not 0.5 - fixed this in casesForICUByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -45,10 +45,10 @@ const covid19ImpactEstimator = (data) => {
          - this.severeCasesByRequestedTime);
       },
       get casesForICUByRequestedTime() {
-        return Math.floor(0.5 * this.infectionsByRequestedTime);
+        return Math.floor(0.05 * this.infectionsByRequestedTime);
       },
       get casesForVentilatorsByRequestedTime() {
-        return Math.floor(0.2 * this.infectionsByRequestedTime);
+        return Math.floor(0.02 * this.infectionsByRequestedTime);
       },
       get dollarsInFlight() {
         return Math.floor(this.infectionsByRequestedTime * input.region.avgDailyIncomePopulation
@@ -68,10 +68,10 @@ const covid19ImpactEstimator = (data) => {
          - this.severeCasesByRequestedTime);
       },
       get casesForICUByRequestedTime() {
-        return Math.floor(0.5 * this.infectionsByRequestedTime);
+        return Math.floor(0.05 * this.infectionsByRequestedTime);
       },
       get casesForVentilatorsByRequestedTime() {
-        return Math.floor(0.2 * this.infectionsByRequestedTime);
+        return Math.floor(0.02 * this.infectionsByRequestedTime);
       },
       get dollarsInFlight() {
         return Math.floor(this.infectionsByRequestedTime * input.region.avgDailyIncomePopulation
